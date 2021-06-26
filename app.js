49bee7ce9082a38dc8053eae4030f4915e1e8efb
PedroIde2005 <84749102+PedroIde2005@users.noreply.github.com>
2021-06-25 23:12:11 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function app() {
+return (
+<div>
+<h1> aula 01 - fundamentos 01</h1>
+</div>
+);
+}
+export default app;
---
+++ index.js
@@ -0,0 +1,9 @@
+import react from 'react';
+import reactDOM from 'react-dom';
+
+import app from './app';
+
+reactdom.render(
+  <app/>,
+  document.getelementbyid('root')
+);
